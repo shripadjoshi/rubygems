6fb4bd2860cb01e25e0274134a1e4ef7d1550ded
Shripad Joshi <shripad.joshi.s@gmail.com>
2020-07-24 09:52:23 +0530

    added the action text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("trix")
+require("@rails/actiontext")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
